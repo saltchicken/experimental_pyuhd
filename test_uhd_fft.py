9003a0c7bc26da70cac00d29e7be217771f76eec
saltchicken <john.eicher89@gmail.com>
2022-08-27 18:51:45 -0700

    Added multiprocessing

--- test_uhd_fft.py
+++ test_uhd_fft.py
@@ -5,14 +5,50 @@ from matplotlib.animation import FuncAnimation
 from scipy.fft import fft, fftfreq, fftshift
 from scipy import signal
 import time
+import multiprocessing
 
-usrp = uhd.usrp.MultiUSRP()
-
-SAMPLE_RATE = 1e6
+SAMPLE_RATE = 50e6
 NUM_SAMPS = 400
 CENTER_FREQ = 104.5e6
 GAIN = 50
 NUM_RECV_FRAMES = 2000
+
+MAX_QUEUE_SIZE = 500
+usrp = uhd.usrp.MultiUSRP()
+
+def plot_processing(q):
+    RUN = True
+    def on_close(event):
+        RUN = False
+        quit()
+    fig, ax = plt.subplots()
+    fig.canvas.mpl_connect('close_event', on_close)
+    ln, = plt.plot([], [], 'r')
+
+    xf = fftshift(fftfreq(NUM_SAMPS, 1 / SAMPLE_RATE) + CENTER_FREQ)
+    ax.set_xlim(min(xf), max(xf))
+    ax.set_ylim(-1, 6)
+
+    while RUN:
+        try:
+            while not q.empty(): 
+                data = q.get()
+                data = data.astype("complex64")
+            window = signal.windows.hann(NUM_SAMPS)
+            yf = fft(data * window)
+            yf = fftshift(yf)
+            yf = np.abs(yf)
+            std = np.std(yf)
+            yf = np.clip(yf, std / 3, std * 100)
+            ln.set_data(xf, yf)
+            #fig.canvas.draw()
+            plt.pause(0.01)
+        except:
+            print("Checking too quick")
+
+q = multiprocessing.Queue( MAX_QUEUE_SIZE )
+plot_process=multiprocessing.Process(None, plot_processing, args=(q,))
+plot_process.start()
 
 usrp.set_rx_rate(SAMPLE_RATE, 0)
 usrp.set_rx_freq(uhd.libpyuhd.types.tune_request(CENTER_FREQ), 0)
@@ -25,29 +61,16 @@ streamer = usrp.get_rx_stream(st_args)
 stream_cmd = uhd.types.StreamCMD(uhd.types.StreamMode.start_cont)
 stream_cmd.stream_now = True
 streamer.issue_stream_cmd(stream_cmd)
-buffer_samps = streamer.get_max_num_samps()
-print(buffer_samps)
+# buffer_samps = streamer.get_max_num_samps()
 recv_buffer = np.zeros((1, NUM_RECV_FRAMES), dtype=np.complex64)
 
-
-fig, ax = plt.subplots()
-ln, = plt.plot([], [], 'r')
-
-xf = fftshift(fftfreq(NUM_SAMPS, 1 / SAMPLE_RATE) + CENTER_FREQ)
-ax.set_xlim(min(xf), max(xf))
-ax.set_ylim(-1, 6)
-
-for t in range(1000):
-    #plt.gca().cla()
-    #Also need to reset limits
-    xf = fftshift(fftfreq(NUM_SAMPS, 1 / SAMPLE_RATE) + CENTER_FREQ)
-
+for t in range(300000):
+    #print(plot_process.is_alive())
     samples = np.zeros((1, NUM_SAMPS), dtype=np.complex64)
     # for i in range(NUM_SAMPS//NUM_RECV_FRAMES):
     #     streamer.recv(recv_buffer, metadata)
     #     samples[i * NUM_RECV_FRAMES:(i + 1) * NUM_RECV_FRAMES] = recv_buffer[0]
-    
-
+    #q.put("test")
     recv_samps = 0
     while recv_samps < NUM_SAMPS:
         samps = streamer.recv(recv_buffer, metadata)
@@ -59,17 +82,18 @@ for t in range(1000):
             samples[:, recv_samps:recv_samps + real_samps] = recv_buffer[:, 0:real_samps]
             recv_samps += real_samps
     
+    if t % 1 == 0:
+        if q.qsize() >= MAX_QUEUE_SIZE:
+            continue
+        else:
+            q.put_nowait(samples)
+            #print(q.qsize())
 
-    window = signal.windows.hann(NUM_SAMPS)
-    yf = fft(samples * window)
-    yf = fftshift(yf)
-    yf = np.abs(yf)
-    std = np.std(yf)
-    yf = np.clip(yf, std / 3, std * 100)
-
-    ln.set_data(xf, yf)
-    #fig.canvas.draw()
-    plt.pause(0.01)
-
+print("Ran the loop")
+plot_process.terminate()
+plot_process.join()
+print("Ending")
 stream_cmd = uhd.types.StreamCMD(uhd.types.StreamMode.stop_cont)
 streamer.issue_stream_cmd(stream_cmd)
+print("Finished Cleanup")
+quit()
