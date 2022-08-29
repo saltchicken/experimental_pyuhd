3b158cac0275a2209ed0cfe05f13e7897c2f12ac
saltchicken <john.eicher89@gmail.com>
2022-08-28 21:41:12 -0700

    Refactored, better performance.

--- multichannel_reading_reference.py
+++ multichannel_reading_reference.py
@@ -6,13 +6,13 @@ from scipy.fft import fft, fftfreq, fftshift
 from scipy import signal
 import time, multiprocessing
 
-SAMPLE_RATE = 20e6
+SAMPLE_RATE = 10e6
 NUM_SAMPS = 400
 CENTER_FREQ = 104.5e6
 GAIN = 50
-NUM_RECV_FRAMES = 2040
+NUM_RECV_FRAMES = 2000
 
-MAX_QUEUE_SIZE = 100 
+MAX_QUEUE_SIZE = 1 
 
 def plot_processing(q):
     fig, ax = plt.subplots()
@@ -28,7 +28,7 @@ def plot_processing(q):
                 data = q.get()
                 data = data.astype("complex64")
             window = signal.windows.hann(NUM_SAMPS)
-            yf = fft(data[-NUM_SAMPS:] * window)
+            yf = fft(data * window)
             yf = fftshift(yf)
             yf = np.abs(yf)
             std = np.std(yf)
@@ -36,7 +36,7 @@ def plot_processing(q):
             ln.set_data(xf, yf)
             return ln,
         except:
-            #print("error with update in plot_processing")
+            print("error with update in plot_processing")
             return ln,
 
     ani = FuncAnimation(fig, update, frames=None, interval=0, blit=True)
@@ -58,18 +58,28 @@ streamer = usrp.get_rx_stream(st_args)
 stream_cmd = uhd.types.StreamCMD(uhd.types.StreamMode.start_cont)
 stream_cmd.stream_now = True
 streamer.issue_stream_cmd(stream_cmd)
-buffer_samps = streamer.get_max_num_samps()
-print(buffer_samps)
-recv_buffer = np.zeros(NUM_RECV_FRAMES, dtype=np.complex64)
+# buffer_samps = streamer.get_max_num_samps()
+recv_buffer = np.zeros((1, NUM_RECV_FRAMES), dtype=np.complex64)
 
 QUEUE_FULL = 0
 QUEUE_WRITTEN = 0
 
 while plot_process.is_alive():
-    samples = np.zeros(NUM_RECV_FRAMES * 20, dtype=np.complex64)
-    for i in range(20):
-        streamer.recv(recv_buffer, metadata)
-        samples[i * NUM_RECV_FRAMES : (i + 1) * NUM_RECV_FRAMES] = recv_buffer
+    samples = np.zeros((1, NUM_SAMPS), dtype=np.complex64)
+    # for i in range(NUM_SAMPS//NUM_RECV_FRAMES):
+    #     streamer.recv(recv_buffer, metadata)
+    #     samples[i * NUM_RECV_FRAMES:(i + 1) * NUM_RECV_FRAMES] = recv_buffer[0]
+    #q.put("test")
+    recv_samps = 0
+    while recv_samps < NUM_SAMPS:
+        samps = streamer.recv(recv_buffer, metadata)
+
+        if metadata.error_code != uhd.types.RXMetadataErrorCode.none:
+            print(metadata.strerror())
+        if samps:
+            real_samps = min(NUM_SAMPS - recv_samps, samps)
+            samples[:, recv_samps:recv_samps + real_samps] = recv_buffer[:, 0:real_samps]
+            recv_samps += real_samps
     
     if q.qsize() >= MAX_QUEUE_SIZE:
         QUEUE_FULL += 1
