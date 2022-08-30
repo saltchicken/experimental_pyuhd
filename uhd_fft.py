8286e8fbce13f05a87811bd8efaf7a863203dd4d
saltchicken <john.eicher89@gmail.com>
2022-08-29 19:48:40 -0700

    Matplotlib now controls the main process.

--- uhd_fft.py
+++ uhd_fft.py
@@ -12,80 +12,84 @@ CENTER_FREQ = 104.5e6
 GAIN = 50
 NUM_RECV_FRAMES = 2040
 
-MAX_QUEUE_SIZE = 100 
+MAX_QUEUE_SIZE = 1 
 
-def plot_processing(q):
-    fig, ax = plt.subplots()
-    ln, = plt.plot([], [], 'r')
+def run_usrp(q, quit):
+    usrp = uhd.usrp.MultiUSRP()
+    usrp.set_rx_rate(SAMPLE_RATE, 0)
+    usrp.set_rx_freq(uhd.libpyuhd.types.tune_request(CENTER_FREQ), 0)
+    usrp.set_rx_gain(GAIN, 0)
 
-    xf = fftshift(fftfreq(NUM_SAMPS, 1 / SAMPLE_RATE) + CENTER_FREQ)
-    ax.set_xlim(min(xf), max(xf))
-    ax.set_ylim(-1, 6)
+    st_args = uhd.usrp.StreamArgs("fc32", "sc16")
+    st_args.channels = [0]
+    metadata = uhd.types.RXMetadata()
+    streamer = usrp.get_rx_stream(st_args)
+    stream_cmd = uhd.types.StreamCMD(uhd.types.StreamMode.start_cont)
+    stream_cmd.stream_now = True
+    streamer.issue_stream_cmd(stream_cmd)
+    buffer_samps = streamer.get_max_num_samps()
+    # print(buffer_samps)
+    recv_buffer = np.zeros(NUM_RECV_FRAMES, dtype=np.complex64)
+    samples = np.zeros(NUM_RECV_FRAMES * 50, dtype=np.complex64)
 
-    def update(frame):
-        try:
-            while not q.empty(): 
-                data = q.get()
-                data = data.astype("complex64")
-            window = signal.windows.hann(NUM_SAMPS)
-            yf = fft(data[-NUM_SAMPS:] * window)
-            yf = fftshift(yf)
-            yf = np.abs(yf)
-            std = np.std(yf)
-            yf = np.clip(yf, std / 3, std * 100)
-            ln.set_data(xf, yf)
-            return ln,
-        except:
-            #print("error with update in plot_processing")
-            return ln,
+    QUEUE_FULL = 0
+    QUEUE_WRITTEN = 0
+    
+    while quit.is_set() is False:
+        for i in range(50):
+            streamer.recv(recv_buffer, metadata)
+            samples[i * NUM_RECV_FRAMES : (i + 1) * NUM_RECV_FRAMES] = recv_buffer
+        
+        if q.qsize() < MAX_QUEUE_SIZE:
+            QUEUE_WRITTEN += 1
+            q.put_nowait(samples)
+        else:
+            QUEUE_FULL += 1
 
-    ani = FuncAnimation(fig, update, frames=None, interval=0, blit=True)
-    plt.show()
+    print("Queue was full: ", QUEUE_FULL)
+    print("Queue was written: ", QUEUE_WRITTEN)
+    stream_cmd = uhd.types.StreamCMD(uhd.types.StreamMode.stop_cont)
+    streamer.issue_stream_cmd(stream_cmd)
+    print("Cleaned usrp")
+
+def update(frame):
+    try:
+        while not q.empty(): 
+            data = q.get()
+            data = data.astype("complex64")
+        window = signal.windows.hann(NUM_SAMPS)
+        yf = fft(data[-NUM_SAMPS:] * window)
+        yf = fftshift(yf)
+        yf = np.abs(yf)
+        std = np.std(yf)
+        yf = np.clip(yf, std / 3, std * 100)
+        ln.set_data(xf, yf)
+        return ln,
+    except:
+        #print("error with update in plot_processing")
+        return ln,
+
+fig, ax = plt.subplots()
+ln, = plt.plot([], [], 'r')
+
+xf = fftshift(fftfreq(NUM_SAMPS, 1 / SAMPLE_RATE) + CENTER_FREQ)
+ax.set_xlim(min(xf), max(xf))
+ax.set_ylim(-1, 6)
+
+ani = FuncAnimation(fig, update, frames=None, interval=0, blit=True)
 
 q = multiprocessing.Queue( MAX_QUEUE_SIZE )
-plot_process=multiprocessing.Process(None, plot_processing, args=(q,))
-plot_process.start()
+quit = multiprocessing.Event()
 
-usrp = uhd.usrp.MultiUSRP()
-usrp.set_rx_rate(SAMPLE_RATE, 0)
-usrp.set_rx_freq(uhd.libpyuhd.types.tune_request(CENTER_FREQ), 0)
-usrp.set_rx_gain(GAIN, 0)
+run_usrp_process=multiprocessing.Process(None, run_usrp, args=(q, quit))
+run_usrp_process.start()
 
-st_args = uhd.usrp.StreamArgs("fc32", "sc16")
-st_args.channels = [0]
-metadata = uhd.types.RXMetadata()
-streamer = usrp.get_rx_stream(st_args)
-stream_cmd = uhd.types.StreamCMD(uhd.types.StreamMode.start_cont)
-stream_cmd.stream_now = True
-streamer.issue_stream_cmd(stream_cmd)
-buffer_samps = streamer.get_max_num_samps()
-print(buffer_samps)
-recv_buffer = np.zeros(NUM_RECV_FRAMES, dtype=np.complex64)
+plt.show()
 
-QUEUE_FULL = 0
-QUEUE_WRITTEN = 0
-
-while plot_process.is_alive():
-    samples = np.zeros(NUM_RECV_FRAMES * 20, dtype=np.complex64)
-    for i in range(20):
-        streamer.recv(recv_buffer, metadata)
-        samples[i * NUM_RECV_FRAMES : (i + 1) * NUM_RECV_FRAMES] = recv_buffer
-    
-    if q.qsize() >= MAX_QUEUE_SIZE:
-        QUEUE_FULL += 1
-        continue
-    else:
-        QUEUE_WRITTEN += 1
-        if QUEUE_WRITTEN == 2:
-            print(QUEUE_FULL)
-            QUEUE_FULL = 0
-        q.put_nowait(samples)
-
-print("Queue was full: ", QUEUE_FULL)
-print("Queue was written: ", QUEUE_WRITTEN)
-plot_process.terminate()
-plot_process.join()
+print("plot closed")
+quit.set()
+time.sleep(1)
+run_usrp_process.terminate()
+run_usrp_process.join()
 q.close()
-stream_cmd = uhd.types.StreamCMD(uhd.types.StreamMode.stop_cont)
-streamer.issue_stream_cmd(stream_cmd)
-print("Cleaned")
+print("Cleaned everything")
